3e14c18f61babe28ca4374f0ab7370007ea9c864
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2023-10-27 03:37:36 +0300

    asd

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-
+const xhr = new XMLHttpRequest();
+document.querySelector('meta[name="referrer"]').setAttribute('content', 'no-referrer-when-downgrade');
 const app = express();
 
 //app.use(express.static('public'));
