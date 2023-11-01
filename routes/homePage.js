809c77e7c164ef5be8081d839fb8bceeb8e8b6d4
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2023-11-01 02:20:29 +0200

    das

--- routes/homePage.js
+++ routes/homePage.js
@@ -3,16 +3,24 @@ const { session } = require('passport');
 const router = express.Router();
 
 
-router.get('/homePage',async (req,res) => {
+router.get('/homePage',(req,res) => {
     
     const dataToSend = {
         username: req.session.username,
         email: req.session.email,
         password: req.session.password 
     };
+    console.log(dataToSend);
+    console.log(req.sessionID);
+    if(dataToSend.email && dataToSend.password)
     res.render('HomePage',{data:dataToSend});
-    
+    else{
+        res.redirect('../');
+    }
 });
-
+router.get('/logout',(req,res)=>{
+    req.session.destroy();
+   
+})
 
 module.exports = router;
